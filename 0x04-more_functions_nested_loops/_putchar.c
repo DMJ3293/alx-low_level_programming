3e140c374fb87e1554ae82219e901ee46c7b7a1f
DMJ3293 <sulaimon.akinyemi952@gamil.com>
2022-10-21 11:05:24 -0700

    Task 3

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -4,7 +4,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: on success 1 else -1
+ *
+ * Return: on suiccess 1.
+ * on error, -1 is returned, and error is set appropriately.
  */
 int _putchar(char c)
 {
